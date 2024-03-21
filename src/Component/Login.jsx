93ab7b8ae11737a0bc96d3b23228d82da8de07ac
KnowldegeWork01 <apoorvkumar_g.int@knowledgew.com>
2024-03-21 15:10:33 +0530

    updated login

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,44 +1,118 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Logo from "../images/signInLogo.jpeg";
+import BG from "../images/pexels-jess-bailey-designs-965119.jpg";
+import { useState } from "react";
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+const defaultTheme = createTheme();
 
- function Login() {
+const Login = () => {
+  const [inputValue, setInputValue] = useState({ email: "", password: "" });
+  const handleFieldChange = (event) => {
+    const { name, value } = event.target;
+    setInputValue((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("LOGIN CREDENTIAL", inputValue);
+  };
+
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Typography
+      style={{
+        // backgroundImage: `url(${BG})`,
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography>
+              <img src={Logo} alt="Image Error"  style={{ marginTop: "5rem" }} />
+            </Typography>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                // required
+                fullWidth
+                id="email"
+                label="Userid"
+                name="email"
+                value={inputValue.email}
+                onChange={handleFieldChange}
+                autoComplete="email"
+              />
+              <TextField
+                margin="normal"
+                // required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                value={inputValue.password}
+                onChange={handleFieldChange}
+                id="password"
+                autoComplete="current-password"
+              />
+              {/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid
+                  item
+                  xs
+                  style={{ display: "flex", justifyContent: "flex-end" }}
+                >
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </Typography>
   );
-}
-
-export default Login
+};
+export default Login;
